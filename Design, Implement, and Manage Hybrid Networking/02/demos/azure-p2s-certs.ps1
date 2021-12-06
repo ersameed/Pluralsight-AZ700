871e47efbb355b5d5a3a57e76c0dcaefc273000f
hgure <62731732+hgure@users.noreply.github.com>
2021-12-06 14:07:20 -0500

    upload 2

---
+++ Design, Implement, and Manage Hybrid Networking/02/demos/azure-p2s-certs.ps1
@@ -0,0 +1,28 @@
+# Generate Azure P2S Certificates
+
+# Ref: timw.info/0tv
+
+# Self-signed root certificate
+$cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+  -Subject "CN=P2SRootCert" -KeyExportPolicy Exportable `
+  -HashAlgorithm sha256 -KeyLength 2048 `
+  -CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign
+
+# Client certificate
+New-SelfSignedCertificate -Type Custom -DnsName P2SChildCert -KeySpec Signature `
+  -Subject "CN=P2SChildCert" -KeyExportPolicy Exportable `
+  -HashAlgorithm sha256 -KeyLength 2048 `
+  -CertStoreLocation "Cert:\CurrentUser\My" `
+  -Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
+
+<#
+- Export the root certificate public key (.cer)
+- Export, no private key, Base-64
+#>
+
+<#
+- Export the client certificate (to install on other computers)
+- Export, include private key, PFX
+#>
+
+
